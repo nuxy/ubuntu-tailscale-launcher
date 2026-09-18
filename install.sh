84c95032585b3569bfb75c997b9a8b5a2abecd16
Marc S. Brooks <marc@mbrooks.info>
2026-09-18 20:38:27 +0800

    Added gnome-terminal dependency check
    Install package on confirmation

--- install.sh
+++ install.sh
@@ -19,9 +19,28 @@
 BASE_DIR=/opt/ubuntu-tailscale-launcher
 TEMP_OUT=/tmp/master.zip
 
+version=`awk -F= '$1=="VERSION_ID" { print $2; }' /etc/os-release | sed 's/^"\([0-9]*\).*/\1/'`
+
+# Check package dependencies
+if [ $version -lt 25 ] && [ -z `which gnome-terminal` ]; then
+  cat << EOF
+**ATTENTION**
+gnome-terminal is not supported in this Ubuntu release and will be installed.
+
+EOF
+
+  read -e -p "Continue? [y/n] " confirm
+  [[ "$confirm" == [Yy]* ]] && echo || exit 0
+
+  sudo apt install -y gnome-terminal
+fi
+
+# Install package sources
 sudo curl -L https://github.com/nuxy/ubuntu-tailscale-launcher/archive/refs/heads/master.zip -o $TEMP_OUT
 sudo unzip -jo $TEMP_OUT -d $BASE_DIR
 sudo rm -f $TEMP_OUT
 sudo cp $BASE_DIR/tailscale.desktop $HOME/.local/share/applications
 
-echo "Installed successfully"
+if [ $? -eq 0 ]; then
+  echo "Installed successfully"
+fi
